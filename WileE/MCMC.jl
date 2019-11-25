1cf8dea00bde31329a1c4b7caa289fedaf75e8fa
mcreel <michael.creel@uab.es>
2019-11-25 06:47:13 +0100

    all working, snapshot

--- WileE/MCMC.jl
+++ WileE/MCMC.jl
@@ -1,52 +1,29 @@
-# This does MCMC, but filtering the statistics through the trained net,
-# to reduce dimension to the minimum needed for identification
+# This does MCMC, either using raw statistic, or using NN transform,
+# depending on the argument usenn
 using SV, Flux, Econometrics, LinearAlgebra, Statistics, DelimitedFiles
 using BSON:@load
 include("lib/transform.jl")
+include("lib/lnL.jl")
 global const info = readdlm("transformation_info")
 
-# specialized likelihood for MCMC using net
-function logL_NN(θ, m, n, burnin, S, model, withdet=true)
-    k = size(m,1)
-    ms = zeros(S, k)
-    Threads.@threads for s = 1:S
-        ms[s,:] = Float64.(model(transform(WileE_model(θ[:])', info)').data)
-    end
-    mbar = mean(ms,dims=1)[:]
-    Σ = cov(ms)
-    x = (m .- mbar)[:]
-    lnL = -0.5*log(det(Σ)) - 0.5*x'*inv(Σ)*x # for Bayesian
-    #=
-    if ~any(isnan.(mbar))
-        Σ = cov(ms)
-        x = (m .- mbar)[:]
-        lnL = try
-            if withdet
-                lnL = -0.5*log(det(Σ)) - 0.5*x'*inv(Σ)*x # for Bayesian
-            else    
-                lnL = 0.5*x'*inv(Σ)*x # for classic indirect inference (note sign change)
-            end    
-        catch
-            lnL = -Inf
-        end
-     else
-         lnL = -Inf
-     end
-     =#
-     return lnL
-end
-
-function MSM_MCMC_NN(m)
+function MSM_MCMC(m, usenn)
     # get the trained net
     @load "best.bson" model
     S = nSimulationDraws # number of simulations
-    m = transform(m', info)
-    m = model(m').data
-    m = Float64.(m)
+    if usenn
+        m = transform(m', info)
+        m = model(m').data
+        m = Float64.(m)
+    end    
     # set up MCMC
     tuning = [0.01, 0.01, 0.01] # fix this somehow
-    θinit = m # use the NN fit as initial θ
-    lnL = θ -> logL_NN(θ, m, n, S, burnin, model)
+    if usenn
+        θinit = m # use the NN fit as initial θ
+        lnL = θ -> LL(θ, m, S, model)
+    else
+        θinit = (ub+lb)/2.0
+        lnL = θ -> LL(θ, m, S)
+    end
     Prior = θ -> prior(θ, lb, ub) # uniform, doesn't matter
     # define things for MCMC
     verbosity = false
@@ -55,6 +32,7 @@ function MSM_MCMC_NN(m)
     chain = mcmc(θinit, ChainLength, burnin, Prior, lnL, Proposal, verbosity)
     # now use a MVN random walk proposal with updates of covariance and longer chain
     # on final loop
+    θinit = mean(chain[:,1:3],dims=1)[:]
     Σ = NeweyWest(chain[:,1:3])
     tuning = 1.0
     ChainLength = 800
@@ -65,6 +43,7 @@ function MSM_MCMC_NN(m)
         if j == MC_loops
             ChainLength = 1600
         end    
+        θinit = mean(chain[:,1:3],dims=1)[:]
         chain = mcmc(θinit, ChainLength, 0, Prior, lnL, Proposal, verbosity)
         if j < MC_loops
             accept = mean(chain[:,end])
--- WileE/RunMe.jl
+++ WileE/RunMe.jl
@@ -4,32 +4,30 @@ include("Analyze.jl")
 
 function RunProject()
 
-mcreps = 10
+# generate the raw training data
+#include("MakeData.jl")
+
+# transform the raw statistics, and split out params and stats
+#include("Transform.jl")
+## when this is done, can delete raw_data.bson
+
+# train the net using the transformed training/testing data
+#include("Train.jl")
+# when this is done, can delete cooked_data.bson
+include("MCMC.jl")
+
+mcreps = 1000
 results_raw = zeros(mcreps,12)
 results_NN = zeros(mcreps,12)
 for mcrep = 1:mcreps
     # generate a draw at true params
     m = WileE_model(θtrue)    
-
-    # generate the raw training data
-    #include("MakeData.jl")
-
-    # transform the raw statistics, and split out params and stats
-    #include("Transform.jl")
-    ## when this is done, can delete raw_data.bson
-
-    # train the net using the transformed training/testing data
-    #include("Train.jl")
-    # when this is done, can delete cooked_data.bson
-
     # do full statistic MSM Bayesian estimation
-    include("MSM_MCMC_raw.jl")
-    chain = MSM_MCMC_raw(m)
+    chain = MCMC(m, false)
     results_raw[mcrep,:] = Analyze(chain)
 
     # do NN statistic MSM Bayesian estimation
-    include("MSM_MCMC_NN.jl")
-    chain = MSM_MCMC_NN(m)
+    chain = MSM_MCMC(m, true)
     results_NN[mcrep,:] = Analyze(chain)
     println("____________________________")
     println("Results so far, raw stat")
@@ -37,8 +35,8 @@ for mcrep = 1:mcreps
     println("Results so far, NN stat")
     dstats(results_NN[1:mcrep,:])
     println("____________________________")
-    writedlm("results_raw", results_raw)
-    writedlm("results_NN", results_NN)
 end
+writedlm("results_raw", results_raw)
+writedlm("results_NN", results_NN)
 end
 RunProject()
