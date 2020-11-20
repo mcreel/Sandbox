b7d35e1a733334f6a43c6674cf7310cef1c28183
Michael Creel <michael.creel@uab.es>
2020-11-20 12:37:40 +0100

    all working with simple example

--- TestTuring/Ex3.jl
+++ TestTuring/Ex3.jl
@@ -3,42 +3,37 @@
 using Turing
 using StatsPlots, Statistics, Optim
 using LinearAlgebra:cholesky,I
-
-
-@model function L(z, Σ)
+@model function L(z, P, Σ)
     # priors for parameters
-    m1 ~ Normal(0,5)
-    m2 ~ Normal(0,5)
-    n, S = 100, 10
-    m = auxstat((m1, m2), n, S) # sample is S repetitions of a single sample
+    μ1 ~ Normal(0,5)
+    μ2 ~ Normal(0,5)
+    m = auxstat((μ1, μ2), P, 10) # sample is S repetitions of a single sample
     z ~ MvNormal(m, Σ*(1+1/S))
 end
 
 # the dgp is just two MVN random vars
 # the statistic is the quantiles of each, and the correlation
 function auxstat(θ, P, S)
-    m1, m2 = θ
+    μ1, μ2 = θ
     n = 100
-    data = [m1 m2] .+ randn(n*S,2)*P
-    q = vcat(quantile(data[:,1], [0.1, 0.5, 0.9]), quantile(data[:,2], [0.1, 0.5, 0.9]), cor(data[:,1], data[:,2])) 
+    data = [μ1 μ2] .+ randn(n*S,2)*P
+    vcat(quantile(data[:,1], [0.1, 0.5, 0.9]), quantile(data[:,2], [0.1, 0.5, 0.9]), cor(data[:,1], data[:,2])) 
 end
 
 # stuff for the dgp
-θ⁰ = (randn(), randn())  # true parameters
+θ⁰ = (1.0, 1.0)  # true parameters
 Ω = [2.0 1.0; 1.0 3.0] # true variance of z
 P = cholesky(Ω).U
 # the real sample value of statistic
 z = auxstat(θ⁰, P, 1)
 # initial quasi-likelihood with identity weight
-model = L(z,I)
-chain = sample(model, HMC(0.1, 5), 10000)
-# get the posterior point estimator
-θbar = mean.((chain[:m1], chain[:m2]))
-θ50 = median.((chain[:m1], chain[:m2]))
-@show θ⁰
-@show θbar
-@show θ50
-#=
+model = L(z,P, I)
+# do the sampling
+#chain = sample(model, HMC(0.1, 5), 1000)
+chain = sample(model, NUTS(0.65), 1000)
+# get the posterior point estimators
+θbar = mean.((chain[:μ1], chain[:μ2]))
+θ50 = median.((chain[:μ1], chain[:μ2]))
 # compute the estimator of covariance of statistic
 S = 100
 zs = zeros(S, size(z,1))
@@ -46,15 +41,14 @@ for s = 1:S
     zs[s,:] = auxstat(θbar, P, 1)
 end
 # second round with efficient weight
-model2 = SNM(z,cov(zs))
-chain = sample(model2, HMC(0.1, 5), 1000)
-θbar = mean.((chain[:m1], chain[:m2]))
-θ50 = median.((chain[:m1], chain[:m2]))
-=#
-
+model = L(z,P, cov(zs))  # set the likehood
+#chain = sample(model, HMC(0.1, 5), 1000)
+chain = sample(model, NUTS(0.65), 1000)
+θbar = mean.((chain[:μ1], chain[:μ2]))
+θ50 = median.((chain[:μ1], chain[:μ2]))
+# optimize(model, MLE(), LBFGS())
 # Summarise results
-describe(chain)
-
+@show chain
 # Plot and save results
 p = plot(chain)
 display(p)
@@ -62,5 +56,4 @@ display(p)
 @show θbar
 @show θ50
 nothing
-# how to do MLE (probably want posterior median for first round estimate)
-# optimize(model, MLE())
+
