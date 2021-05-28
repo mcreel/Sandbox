0c5c3462f923c56a0609b8d2c1b87b15b33ab8e1
Michael Creel <michael.creel@uab.es>
2021-05-28 17:53:35 +0200

    snap

--- SolveDSGE/CK_solution_file.jl
+++ SolveDSGE/CK_solution_file.jl
@@ -2,9 +2,7 @@ using SolveDSGE
 
 filename = "CK.txt"
 path = joinpath(@__DIR__,filename)
-
 process_model(path)
-
 processed_filename = "CK_processed.txt"
 processed_path =  joinpath(@__DIR__,processed_filename)
 
@@ -12,27 +10,31 @@ dsge = retrieve_processed_model(processed_path)
 
 
 # convert this to a function that computes SS given parameters
+function params()
+    α = 0.33
+    β = 0.99
+    δ = 0.025
+    γ = 2.0
+    ρ₁ = 0.9
+    σ₁ = 0.02 
+    ρ₂ = 0.7
+    σ₂ = 0.01
+    nss = 1.0/3.0
+    [α, β, δ, γ, ρ₁, σ₁, ρ₂, σ₂, nss]
+end   
+
 function CKss()
-alppha = 0.33
-betta = 0.99
-delta = 0.025
-gam = 2.0
-rho1 = 0.9
-sigma1 = 0.02 
-rho2 = 0.7
-sigma2 = 0.01
-nss = 1.0/3.0
-#alppha, betta, delta, gam, rho1, sigma1, rho2, sigma2, nss = params
-c1 = ((1/betta + delta - 1)/alppha)^(1/(1-alppha))
-kss = nss/c1
-iss = delta*kss
-yss = kss^alppha * nss^(1-alppha)
-css = yss - iss;
-MUCss = css^(-gam)
-rss = alppha  * kss^(alppha-1) * nss^(1-alppha)
-wss = (1-alppha)* (kss)^alppha * nss^(-alppha)
-MULss = wss*MUCss
-[0.0, 0.0, kss, kss, yss, css, nss, iss, MUCss, MULss, rss, wss]
+    α, β, δ, γ, ρ₁, σ₁, ρ₂, σ₂, nss = params()
+    c1 = ((1/β  + δ  - 1)/α )^(1/(1-α))
+    kss = nss/c1
+    iss = δ*kss
+    yss = kss^α * nss^(1-α)
+    css = yss - iss
+    MUCss = css^(-γ)
+    rss = α * kss^(α-1) * nss^(1-α)
+    wss = (1-α)* (kss)^α * nss^(-α)
+    MULss = wss*MUCss
+    [0.0, 0.0, kss, kss, yss, css, nss, iss, MUCss, MULss, rss, wss]
 end
 
 #= Use this to verify steady state
@@ -40,6 +42,6 @@ tol = 1e-8
 maxiters = 1000
 ss = compute_steady_state(dsge, CKss(), tol, maxiters)
 =#
-NNN = PerturbationScheme(CKss(), 1.0, "third")
-soln_to = solve_model(dsge,NNN)
+scheme = PerturbationScheme(CKss(), 1.0, "third")
+solution = solve_model(dsge, scheme)
 
