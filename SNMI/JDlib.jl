351875e6b0973eab077dbfbfbcadfb2559bafa59
mcreel <michael.creel@uab.es>
2021-08-15 10:45:12 +0200

    new stats

--- SNMI/JDlib.jl
+++ SNMI/JDlib.jl
@@ -97,7 +97,7 @@ end
     nojump[1:2] .= true
     # jump stats 
     jumpsize = mean(RV[jump]) - mean(BV[jump])
-    jumpsize2 = std(rets[jump]) - std(rets[nojump])
+    jumpsize2 = log.(std(rets[jump]) .+ 1.0) - log.(std(rets[nojump]) .+ 1.0) # limit outliers
     njumps = mean(jump[3:end])
     # ρ
     X = [ones(n-1) BV[2:end] BV[1:end-1]]
@@ -112,7 +112,7 @@ end
     βvol = X\y
     ϵvol = y-X*βvol
     σvol = std(ϵvol)
-    κvol = std(ϵvol.^2.0)
+    κvol = log.(std(ϵvol.^2.0) .+ 1.0)  # limit outliers
     # jump size
     X = [ones(n) jump BV jump.*BV]
     y = RV
@@ -125,10 +125,6 @@ end
     qs2 = quantile(RV,[0.5, 0.9])
     qs3 = quantile(BV,[0.5, 0.9])
     sqrt(n)*vcat(βrets, βvol, βjump,σrets, σvol, σjump,κrets, κvol, κjump, mean(RV) - mean(BV), jumpsize, jumpsize2, qs[2]/qs[1], qs2[2]/qs2[1], qs3[2]./qs3[1], qs2 ./ qs3, njumps)
-
-
-
-# 11, 14, 19 are ones with outliers, especially 14
 
     # brets 1:3
     # bvol 4:6
