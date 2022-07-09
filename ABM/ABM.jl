dc7330b9f77eb6f3638401f6893ccc2b8e1d0311
Michael Creel <michael.creel@uab.es>
2022-07-09 09:32:36 +0200

    update

--- ABM/ABM.jl
+++ ABM/ABM.jl
@@ -1,3 +1,14 @@
+# Simulates model from
+# Alfarano, S., Lux, T., & Wagner, F. (2008). Time variation 
+# of higher moments in a financial market with heterogeneous
+# agents: An analytical approach. Journal of Economic Dynamics
+# and Control, 32(1), 101-136. 
+#
+# References to equations follow 
+# Chen, Zhenxi, and Thomas Lux. "Estimation of sentiment 
+# effects in financial markets: A simulated method of moments
+# approach." Computational Economics 52, no. 3 (2018): 711-744.
+
 using Distributions, StatsPlots
 
 @views function alw(a=0.0003, b=0.0014, σf=0.03; T=20000, burnin=5000)
@@ -8,25 +19,28 @@ ns = zeros(T+1) # container
 t = -burnin # initial continuous time
 dt = 1 # discrete time at start of recording
 while t < T+1
-    # rates
+    # rates: from eq. 3, first expression. (second expression is in error)
     π⁺ = (Nc - n)*(a + b*n) # rate of positive switches
     π⁻ = n*(a + b*(Nc - n)) # rate of negative switches
-    # wait time for state change
+    # wait time for state change (Julia uses scale, not rate)
     λ = n*π⁻  + (Nc - n)*π⁺ # rate of state change
-    Δt = rand(Exponential(1.0/λ)) # random wait time for state change
-    t += Δt # current time
+    Δt = rand(Exponential(1.0/λ)) 
+    # time of state change
+    t += Δt 
     if t > dt
         ns[dt] = n # record n before changing it
         dt +=1 # increment trading day
     end
     # change state
-    rand() < n*π⁻/λ ? n -=1 : n +=1
+    rand() < π⁻/(π⁻ + π⁺) ? n -=1 : n += 1 # uses prob. that one of two exponentials occurs first
 end
-x = 2.0 .* ns./Nc .- 1.0  # defined in paragraph following eq. 2
-# returns, defined in CL2018 eq. 9 (note that NcVc/(NfVf) is set to 1,
+# sentiment index, defined in paragraph following eq. 2
+x = 2.0 .* ns ./ Nc .- 1.0
+# returns, defined in eq. 9 (note that NcVc/(NfVf) is set to 1,
 # as discussed in paragraph following Table 1
 rets = σf .* randn(T) + (x[2:end] - x[1:end-1]) 
-return rets
+return rets, x, ns
 end
-r = alw()
-plot(r)
+rets, x, ns =  alw()
+plot(rets)
+density(x)
